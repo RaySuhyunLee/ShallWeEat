946be1888c8358f289185381a80b7caee7c8a6f7
gustnxodjs <gustnxodjs@snu.ac.kr>
2015-04-25 22:10:00 +0900

    do signup but not finished error_message

---
+++ ShallWeEat/app/controllers/accounts_controller.rb
@@ -0,0 +1,26 @@
+class AccountsController < ApplicationController
+  def signup
+		@account = Account.new
+  end
+
+	def create
+		@account = Account.new(account_params)
+		if @account.save
+			redirect_to @account
+		else
+			redirect_to :back
+		end
+	end
+
+	def show
+		@account = Account.find(params[:id])
+
+	end
+
+	private
+		def account_params
+			params.require(:account).permit(:name,:pass, :gender, :birth, :email)
+		end
+
+
+end
