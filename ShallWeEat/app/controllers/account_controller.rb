a53e736dfabd3fa66c8a6fbb48c1f32075ebcdb7
Suhyun Lee <raysuhyunlee@gmail.com>
2015-04-25 15:16:47 +0900

    Implemented password change form

--- ShallWeEat/app/controllers/account_controller.rb
+++ ShallWeEat/app/controllers/account_controller.rb
@@ -1,2 +1,9 @@
 class AccountController < ApplicationController
+	def edit
+		@user = Account.new(name: 'test', pass: 'test', email: 'test')
+	end
+	def edit_profile
+		#@user = Users.find_by name: ''
+		@user = Account.new(name: 'test')
+	end
 end
