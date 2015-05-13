5360cb75694653266f5504668a5cf87c9d4f666a
gustnxodjs <gustnxodjs@snu.ac.kr>
2015-05-13 21:10:34 +0900

    complete search_food method! => input is ANN ouput arrat and rank of approprivate food, output is [name, image_source]

--- ShallWeEat/app/controllers/suggestions_controller.rb
+++ ShallWeEat/app/controllers/suggestions_controller.rb
@@ -35,8 +35,7 @@ class SuggestionsController < ApplicationController
 #		redirect_to :suggestions_get_suggestion
 #	end
 
-	def search_food(input_list)
-	{
+	def search_food(input_list, rank)
 		def abs(number)
 			if(number > 0)
 				number
@@ -45,11 +44,38 @@ class SuggestionsController < ApplicationController
 			end
 		end
 
+		@i = 0
+		@answer = []
 
+		Food.all.each do |food|
+			difference_value = 
+				abs(food.spicy - input_list[0]) + 
+				abs(food.sour - input_list[1]) + 
+				abs(food.sweet - input_list[2]) + 
+				abs(food.salty - input_list[3]) + 
+				abs(food.bitter - input_list[4]) + 
+				abs(food.hot - input_list[5]) + 
+				abs(food.cold - input_list[6]) + 
+				abs(food.calories - input_list[7]) + 
+				abs(food.time - input_list[8]) + 
+				abs(food.price - input_list[9])
 
+			@answer[@i] = [difference_value,(@i+1)] 
+			@i = @i + 1
+		end
+		
+		@answer = @answer.sort
 
-	}
+		#rank = 1 -> most appropriate food
+		
+		food_answer = Food.find(@answer[rank - 1][1]) 
+	
+		name = food_answer.name #name is food'name
+		image_src = food_answer.image #image_src is string that food image route
 
+		
+			return [name, image_src]
+	end
 
 
 
