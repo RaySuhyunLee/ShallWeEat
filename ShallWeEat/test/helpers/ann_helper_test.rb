dc64fc6f4186199dcc89db1cf15ae23b7545c4c5
Beatriz Santos <bsantosb@snu.ac.kr>
2015-06-09 20:59:47 +0900

    tests commits

--- ShallWeEat/test/helpers/ann_helper_test.rb
+++ ShallWeEat/test/helpers/ann_helper_test.rb
@@ -1,15 +1,30 @@
-require 'test_helper'
-#require 'test/unit'
+#require 'test_helper'
+require 'test/unit'
 
-class AnnHelperTest < ActionView::TestCase
-#class AnnHelperTest < Test::Unit::TestCase
+#class AnnHelperTest < ActionView::TestCase
+
+#class MyShortcut < RubyFann::Shortcut
+ # def initialize
+  #  super(:num_inputs=>4, :num_outputs=>10)    
+ # end
+#end
+
+
+class AnnHelperTest < Test::Unit::TestCase
 
 
 #test "#init_fann" do
-	#super(:num_inputs=>4, :num_outputs=>10)    
+#	super(:num_inputs=>4, :num_outputs=>10)    
 #	fann = RubyFann::Standard.new(:num_inputs=>4, :hidden_neurons=>[5, 6, 7, 8], :num_outputs=>10)    
  #   assert(fann)
 #end
+
+#def test_init_fann
+#	super(:num_inputs=>4, :num_outputs=>10)    
+#	fann = RubyFann::Standard.new(:num_inputs=>4, :hidden_neurons=>[5, 6, 7, 8], :num_outputs=>10)    
+#    assert(fann)
+#end
+
 
 #test "#test_save_fann" do
   #fann.save('suggestion_ann.net')
